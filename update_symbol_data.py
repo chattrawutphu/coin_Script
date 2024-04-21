52b392ac66e35fc6beb3857c387ea68b65004889
chattrawutphu <chattrawut.phoolakorn@gmail.com>
2024-04-21 17:25:46 +0700

    update symbol data function

--- update_symbol_data.py
+++ update_symbol_data.py
@@ -1,5 +1,42 @@
+import ast
 import json
+import os
 import ccxt
+
+import json
+
+def update_symbol_data(filtered_data):
+    try:
+        with open('config.py', 'r') as config_file:
+            lines = config_file.readlines()
+    except FileNotFoundError:
+        lines = []
+
+    symbol_data_index = None
+    for i, line in enumerate(lines):
+        if 'symbol_data' in line:
+            symbol_data_index = i
+            break
+
+    if symbol_data_index is not None:
+        symbol_data = ast.literal_eval(lines[symbol_data_index].split('=')[1].strip())
+    else:
+        symbol_data = []
+
+    symbol_data = filtered_data
+
+    symbol_data_str = json.dumps(symbol_data, separators=(',', ':'))
+    if symbol_data_index is not None:
+        lines[symbol_data_index] = f"symbol_data = {symbol_data_str}{os.linesep}"
+    else:
+        lines.append(f"symbol_data = {symbol_data_str}{os.linesep}")
+
+    with open('config.py', 'w') as config_file:
+        config_file.writelines(lines)
+
+
+
+
 
 exchange = ccxt.binance({
     'apiKey': '6041331240427dbbf26bd671beee93f6686b57dde4bde5108672963fad02bf2e',
@@ -22,6 +59,8 @@ for item in data:
             }
         }
         filtered_data.append(filtered_item)
-print(filtered_data)
-with open('result.json', 'w') as f:
-    json.dump(filtered_data, f, indent=4)
+#print(filtered_data)
+#with open('result.json', 'w') as f:
+#    json.dump(filtered_data, f, indent=4)
+
+update_symbol_data(filtered_data)
